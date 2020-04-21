e242990670cbe18281083ccf365007e61bc9d49c
Jeremy Hearfield <jeremyhearfield@gmail.com>
2020-04-21 23:48:39 +0200

    test4

--- app/src/main/java/com/android/velocis/trainingTajm/MainActivity.java
+++ app/src/main/java/com/android/velocis/trainingTajm/MainActivity.java
@@ -1,13 +1,31 @@
 package com.android.velocis.trainingTajm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button pushup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pushup = (Button) findViewById(R.id.pushup);
+        pushup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }

e77d3a0c9b3b3579d903a48121c88ae5fe33419e
Jeremy Hearfield <jeremyhearfield@gmail.com>
2020-04-22 21:34:46 +0200

    onClick allButton fix 2.0

--- app/src/main/java/com/android/velocis/trainingTajm/MainActivity.java
+++ app/src/main/java/com/android/velocis/trainingTajm/MainActivity.java
@@ -8,22 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button pushup;
-    private Button pullup;
-    private Button dip;
-    private Button squat;
-    private Button plank;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        pushup = (Button) findViewById(R.id.pushup);
-        pullup = (Button) findViewById(R.id.pullup);
-        dip = (Button) findViewById(R.id.dip);
-        squat = (Button) findViewById(R.id.squat);
-        plank = (Button) findViewById(R.id.plank);
 
     }
 
